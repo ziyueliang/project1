a6274320664eb24218e6d01f617d984f0ca47516
ziyueliang <liangziyue0306@126.com>
2019-09-15 18:35:03 -0400

    project 1

---
+++ js/app.js
@@ -0,0 +1,116 @@
+$(document).foundation();
+function onHover1(){
+    $("#hover-icon-1").attr('src', '../image/Icon_1.jpg');
+
+}
+
+function offHover1(){
+    $("#hover-icon-1").attr('src', '../image/Icon_1_1.jpg');
+}
+
+function onHover2(){
+    $("#hover-icon-2").attr('src', '../image/Icon_2.png');
+
+}
+function offHover2(){
+    $("#hover-icon-2").attr('src', '../image/Icon_2_2.jpg');
+
+}
+
+function onHover3(){
+    $("#hover-icon-3").attr('src', '../image/Icon_3.png');
+}
+function offHover3(){
+    $("#hover-icon-3").attr('src', '../image/Icon_3_3.jpg');
+}
+function onHover4(){
+    $("#hover-icon-4").attr('src', '../image/Icon_4.png');
+}
+function offHover4(){
+    $("#hover-icon-4").attr('src', '../image/Icon_4_4.jpg');
+}
+function onHover5(){
+    $("#hover-icon-5").attr('src', '../image/Icon_5.png');
+}
+function offHover5(){
+    $("#hover-icon-5").attr('src', '../image/Icon_5_5.jpg');
+}
+function onHover6(){
+    $("#hover-icon-6").attr('src', '../image/Icon_6.png');
+}
+function offHover6(){
+    $("#hover-icon-6").attr('src', '../image/Icon_6_6.jpg');
+}
+
+function onHover7(){
+    $("#contact-1-1").attr('src', '../image/1.png');
+    $("#contact-1-text").attr('class', 'gray');
+    $("#contact-1-2").attr('src', '../image/contact-1.png');
+
+}
+function offHover7(){
+    $("#contact-1-1").attr('src', '../image/active-1.png');
+    $("#contact-1-text").attr('class', 'blue');
+    $("#contact-1-2").attr('src', '../image/contact-1-active.png');
+}
+
+function onHover8(){
+    $("#contact-2-1").attr('src', '../image/2.png');
+    $("#contact-2-text").attr('class', 'gray');
+    $("#contact-2-2").attr('src', '../image/contact-2:3-hover.png');
+
+}
+function offHover8(){
+    $("#contact-2-1").attr('src', '../image/active-2.png');
+    $("#contact-2-text").attr('class', 'blue');
+    $("#contact-2-2").attr('src', '../image/contact-2-active.png');
+}
+
+function onHover9(){
+    $("#contact-3-1").attr('src', '../image/3.png');
+    $("#contact-3-text").attr('class', 'gray');
+    $("#contact-3-2").attr('src', '../image/contact-2:3-hover.png');
+
+}
+function offHover9(){
+    $("#contact-3-1").attr('src', '../image/active-3.png');
+    $("#contact-3-text").attr('class', 'blue');
+    $("#contact-3-2").attr('src', '../image/contact-3-active.png');
+}
+function onHover10(){
+    $("#contact-4-1").attr('src', '../image/4.png');
+    $("#contact-4-text").attr('class', 'gray');
+    $("#contact-4-2").attr('src', '../image/contact-4-hover.png');
+
+}
+function offHover10(){
+    $("#contact-4-1").attr('src', '../image/active-4.png');
+    $("#contact-4-text").attr('class', 'blue');
+    $("#contact-4-2").attr('src', '../image/contact-4-active.png');
+}
+function onHover11(){
+    $("#footer-hover-1").attr('src', '../image/Avatar_ Facebook.png');
+}
+function offHover11(){
+    $("#footer-hover-1").attr('src', '../image/Default-Facebook.png');
+}
+function onHover12(){
+    $("#footer-hover-2").attr('src', '../image/Avatar_ Foursquare.png');
+}
+function offHover12(){
+    $("#footer-hover-2").attr('src', '../image/Default-Foursquare.png');
+}
+function onHover13(){
+    $("#footer-hover-3").attr('src', '../image/Avatar_ Twitter.png');
+}
+function offHover13(){
+    $("#footer-hover-3").attr('src', '../image/Default-Twitter.png');
+}
+function onHover14(){
+    $("#footer-hover-4").attr('src', '../image/Avatar_ Youtube.png');
+}
+function offHover14(){
+    $("#footer-hover-4").attr('src', '../image/Default-Youtube.png');
+}
+document.getElementById("checkbox3").disabled=true;
+document.getElementById("checkbox4").disabled=true;
